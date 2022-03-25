85f0ce61acf93ebffba633913b0da3d097e54402
devfpy <devfpy@hotmail.com>
2022-03-25 20:02:32 +0800

    Updates

---
+++ precache-manifest.f6a47d4beb095d09bb7667e13ddeaad3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7394bc054e243f7ec311972295dd3787",
+    "url": "./index.html"
+  },
+  {
+    "revision": "032bf77f667989652731",
+    "url": "./static/css/2.82a213cb.chunk.css"
+  },
+  {
+    "revision": "c6df973d733e3db95bb8",
+    "url": "./static/css/main.27649e8b.chunk.css"
+  },
+  {
+    "revision": "032bf77f667989652731",
+    "url": "./static/js/2.34f16b8d.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "./static/js/2.34f16b8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6df973d733e3db95bb8",
+    "url": "./static/js/main.d4089d46.chunk.js"
+  },
+  {
+    "revision": "3e0adfc724325c194548",
+    "url": "./static/js/runtime-main.34a6ea7a.js"
+  }
+]);
